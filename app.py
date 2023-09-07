c97d31b1002ca314283e6b24dde071cb3263c52d
mprintz1234 <printzmaximilian@gmail.com>
2023-09-07 15:11:06 +0800

    cs

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from modules.CalendarScheduling import CalendarScheduling
 
 app = Flask(__name__)
 
-global cs = CalendarScheduling()
+# global cs = CalendarScheduling()
 
 @app.route('/')
 def hello_world():
@@ -13,11 +13,12 @@ def hello_world():
 
 @app.route('/test')
 def test():
-    return cs.get_input()
+    return CalendarScheduling().get_input()
 
 @app.route('/calendar-scheduling', methods = ['POST'])
 def calendarScheduling():
     data = request.get_json()
+    cs = CalendarScheduling()
     cs.set_input(data)
     print(len(data))
     return cs.answer(data)
