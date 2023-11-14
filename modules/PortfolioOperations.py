095e4910eb4a7f39c7a9a7dc25efc1278c0122e2
mprintz1234 <printzmaximilian@gmail.com>
2023-11-14 18:02:37 +0800

    gcc

--- modules/PortfolioOperations.py
+++ modules/PortfolioOperations.py
@@ -3,36 +3,49 @@ class PortfolioOperations():
         ans = []
         for testcase in data["inputs"]:
             maxSum = int(testcase[0].split()[2])
+            biggestCount = 0
             s1 = [int(num) for num in testcase[1].split()]
             s2 = [int(num) for num in testcase[2].split()]
-            #dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
-            s = []
-            currCount = 0
-            if s1:
-                count = s1[0]
-                if count < maxSum:
-                    s.append((count, 1, 1, 0))
-            if s2:
-                count = s2[0]
-                if count < maxSum:
-                    s.append((count, 1, 0, 1))
+            dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+
+            #dp[1][0] = s1[0]
+            # if s1[0] <= maxSum:
+            #     biggestCount = max(biggestCount, 1)
+            #dp[0][1] = s2[0]
+            # if s2[0] <= maxSum:
+            #     biggestCount = max(biggestCount, 1)
+
+            # dp[1][1] = max(s1[0], s2[0])
+            # if dp[1][1] <= maxSum:
+            #     biggestCount = max(biggestCount, 2)
 
 
-            while s:
-                top = s.pop(0)
-                #print(top)
-                if top[1] > currCount:
-                    currCount = top[1]
-                
-                # take from s1
-                if top[2] < len(s1) and top[0] + s1[top[2]] < maxSum:
-                    s.append((top[0] + s1[top[2]], top[1]+1, top[2]+1, top[3]))
+            for i in range(1, len(s1)+1):
+                dp[i][0] = dp[i-1][0] + s1[i-1]
+                # if dp[i][1] <= maxSum:
+                #     biggestCount = max
+            
+            for i in range(1, len(s2)+1):
+                dp[0][i] = dp[0][i-1] + s2[i-1]
 
-                # take from s2
-                if top[3] < len(s2) and top[0] + s2[top[3]] < maxSum:
-                    s.append((top[0] + s2[top[3]], top[1]+1, top[2], top[3]+1))
-            ans.append(currCount)
+            for row in dp:
+                print(row)
+            print()
 
+            for s1_i in range(1, len(s1)+1):
+                for s2_i in range(2, len(s2)+1):
+                    dp[s1_i][s2_i] = max(dp[s1_i-1][s2_i] + s1[s1_i-1], dp[s1_i][s2_i-1] + s2[s2_i-1])
+            for row in dp:
+                print(row)
+            print()
+
+            for s1_i in range(1, len(s1)+1):
+                for s2_i in range(2, len(s2)+1):
+                    if dp[s1_i][s2_i] <= maxSum:
+                        biggestCount = max(biggestCount, s1_i+s2_i)
+            
+            ans.append(biggestCount)
+            
         return {"answer": ans}
 
 # tc = {
