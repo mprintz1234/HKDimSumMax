c0c428dc14b78b86fd245cf24257e4073c09f1c9
mprintz1234 <printzmaximilian@gmail.com>
2023-11-14 16:50:51 +0800

    GCC

---
+++ modules/TimeIntervals.py
@@ -0,0 +1,109 @@
+class TimeIntervals():
+
+    def getResStr(self, start, end, num, lstOfIntervals):
+        emplLst = []
+        for interval in lstOfIntervals:
+            emplLst.append(interval[2])
+        return str(start) + " " + str(end) + " " + str(num) + " " + ' '.join(sorted(emplLst))
+
+    def answer(self, data):
+        
+        ans = []
+
+        for testcase in data["inputs"]:
+            res = [testcase[0]]
+
+            n = int(testcase[0])
+            employees = testcase[1].split()
+            pq = []
+
+            for i in range(2, len(testcase)):
+                interval = testcase[i].split()
+                pq.append((int(interval[0]), int(interval[1]), employees[i-2]))
+
+            pq = sorted(pq)            
+            curr_time = pq[0][0]
+            min_end_interval_time = pq[0][1]
+
+            i = 0
+
+            on_desk_q = {}
+            clock = pq[0][0] + 1
+            start_time = pq[0][0]
+            on_desk_q[start_time] = []
+            while pq and pq[0][0] == start_time:
+                on_desk_q[start_time].append(pq[0])
+                pq.pop(0)                
+
+            while clock <= 21:
+                leave = False
+                join = False
+                combined = [] # get who is currently on desk
+
+                for key in on_desk_q.keys():
+                    combined += on_desk_q[key]
+
+                    # any employee need to leave desk
+                    while on_desk_q[key] and on_desk_q[key][0][1] == clock:
+                        print("user leaving", on_desk_q[key][0])
+                        leave = True
+                        on_desk_q[key].pop(0)
+
+                # add new employee to desk
+                while pq and pq[0][0] == clock:
+                    join = True
+                    print("adding user", pq[0])
+                    # enqueue users to desk
+                    if pq[0][0]in on_desk_q:
+                        on_desk_q[pq[0][0]].append(pq[0])
+                    else:
+                        on_desk_q[pq[0][0]] = [pq[0]]
+                    pq.pop(0)
+
+                if leave or join:
+                    #res.append([start_time, clock, len(combined), combined])
+                    res.append(self.getResStr(start_time, clock, len(combined), combined))
+                    start_time = clock
+
+                clock += 1
+            ans.append(res)
+
+        return {"answer": ans}
+
+tc = {
+    "inputs": [["5",
+        "Alice Bob Cacey Deepak Emma",
+        "10 14",
+        "11 12",
+        "10 15",
+        "12 16",
+        "14 16"],
+        ["3",
+        "Neil Angel Alok",
+        "1 10",
+        "7 9",
+        "7 10"]]
+}
+
+
+# {
+#     "inputs": [
+#         "5",
+#         "Alice Bob Cacey Deepak Emma",
+#         "10 14",
+#         "11 12",
+#         "10 15",
+#         "12 16",
+#         "14 16"
+#     ],
+#     [
+#         "3",
+#         "Neil Angel Alok",
+#         "1 10",
+#         "7 9",
+#         "7 10"
+#     ],
+# }
+
+ti = TimeIntervals()
+print(ti.answer(tc))
