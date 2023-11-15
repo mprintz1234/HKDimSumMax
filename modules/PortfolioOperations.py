1b803d22620a60164c3eeb1b9a8c8fd38a7b8767
mprintz1234 <printzmaximilian@gmail.com>
2023-11-15 11:39:37 +0800

    gcc

--- modules/PortfolioOperations.py
+++ modules/PortfolioOperations.py
@@ -6,7 +6,7 @@ class PortfolioOperations():
             biggestCount = 0
             s1 = [int(num) for num in testcase[1].split()]
             s2 = [int(num) for num in testcase[2].split()]
-            dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+            dp = [[(-1, -1) for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
 
             #dp[1][0] = s1[0]
             # if s1[0] <= maxSum:
@@ -19,30 +19,38 @@ class PortfolioOperations():
             # if dp[1][1] <= maxSum:
             #     biggestCount = max(biggestCount, 2)
 
+            dp[0][0] = (0, 0)
+
 
             for i in range(1, len(s1)+1):
-                dp[i][0] = dp[i-1][0] + s1[i-1]
-                # if dp[i][1] <= maxSum:
-                #     biggestCount = max
+                if dp[i-1][0][0] >= 0 and dp[i-1][0][0] + s1[i-1] <= maxSum:
+                    dp[i][0] = (dp[i-1][0][0] + s1[i-1], dp[i-1][0][1]+1)
+                else:
+                    dp[i][0] = (-1, -1)
             
             for i in range(1, len(s2)+1):
-                dp[0][i] = dp[0][i-1] + s2[i-1]
+                if dp[0][i-1][0] >= 0 and dp[0][i-1][0] + s2[i-1] <= maxSum:
+                    dp[0][i] = (dp[0][i-1][0] + s2[i-1], dp[0][i-1][1]+1)
+                else:
+                    dp[0][i] = (-1, -1)
 
-            for row in dp:
-                print(row)
-            print()
+            # for row in dp:
+            #     print(row)
+            
+            for s1_i in range(1, len(s1)+1):
+                for s2_i in range(1, len(s2)+1):
+                    if dp[s1_i-1][s2_i][0] >= 0 and dp[s1_i-1][s2_i][0] + s1[s1_i-1] <= maxSum:
+                        #dp[s1_i][s2_i] = max(dp[s1_i-1][s2_i] + s1[s1_i-1], dp[s1_i][s2_i-1] + s2[s2_i-1])
+                        dp[s1_i][s2_i] = (dp[s1_i-1][s2_i][0] + s1[s1_i-1], dp[s1_i-1][s2_i][1] + 1)
+                    elif dp[s1_i][s2_i-1][0] >= 0 and dp[s1_i][s2_i-1][0] + s2[s2_i-1] <= maxSum:
+                        dp[s1_i][s2_i] = (dp[s1_i][s2_i-1][0] + s2[s2_i-1], dp[s1_i][s2_i-1][1] + 1)
 
+            # for row in dp:
+            #     print(row)
             for s1_i in range(1, len(s1)+1):
                 for s2_i in range(2, len(s2)+1):
-                    dp[s1_i][s2_i] = max(dp[s1_i-1][s2_i] + s1[s1_i-1], dp[s1_i][s2_i-1] + s2[s2_i-1])
-            for row in dp:
-                print(row)
-            print()
-
-            for s1_i in range(1, len(s1)+1):
-                for s2_i in range(2, len(s2)+1):
-                    if dp[s1_i][s2_i] <= maxSum:
-                        biggestCount = max(biggestCount, s1_i+s2_i)
+                    if dp[s1_i][s2_i][0] != -1:
+                        biggestCount = max(biggestCount, dp[s1_i][s2_i][1])
             
             ans.append(biggestCount)
             
